a362416c545cca7ae52290609fad058fb9f0f405
Voronchikhin_Ivan <steam.voron2015@yandex.ru>
2024-07-04 04:00:43 +0300

    The newest version of the programm without any global variables

--- project1/project1/project1.cpp
+++ project1/project1/project1.cpp
@@ -18,27 +18,15 @@ using namespace System::IO;
 #define HOME 71
 #define END 79
 
-// Массив, содержащий строки с вопросами
-char dan[7][55] = { 
- "Какой пловец набрал больше всего очков?               ",
- "Есть ли пловцы, набравшие одинаковое количество очков?",
- "Сколько пловцов улучшили свои результаты?             ",
- "Кто стал лучшим на дистанции:                         ",
- "Алфавитный список участников соревнований             ",
- "Диаграмма по набранным очкам в процентном соотношении ",
- "Выход                                                 "}; 
- // Массив из пробелов для оформления
-char BlankLine[]="                                                        "; 
-
 int menu(int); // указание шаблонов используемых функций
 void maxim(struct z*);
 float text_to_time(char *);
 void kolvo(struct z*);
 void match(struct z*);
-void alfalist(struct z*);
-void vstavka(struct z*, char*);
+void alfalist(struct z*, struct sp*);
+void vstavka(struct z*, char*,  struct sp**);
 void best_on_dist(struct z*);
-void diagram(struct z*);
+void diagram(struct z*, struct sp*);
 
 struct z {
 	char name[20]; // ФИО спортсмена
@@ -61,7 +49,17 @@ struct sp {
 int main(array<System::String ^> ^args)
 {
 	int i, n, NC;
-	FILE *in; // на локальном уровне осуществляем открытие файла,
+	char dan[7][55] = { 
+ "Какой пловец набрал больше всего очков?               ",
+ "Есть ли пловцы, набравшие одинаковое количество очков?",
+ "Сколько пловцов улучшили свои результаты?             ",
+ "Кто стал лучшим на дистанции:                         ",
+ "Алфавитный список участников соревнований             ",
+ "Диаграмма по набранным очкам в процентном соотношении ",
+ "Выход                                                 "}; 
+    // Массив из пробелов для оформления
+    char BlankLine[] = "                                                        "; 
+	FILE *in; // на локальном уровне осуществляем открытие файла
 	struct z *swimmers; // считываем данные из файла в выделенную память
 	//setlocale(LC_CTYPE, "Russian");
 	system("chcp 1251>nul"); // установление кодировки 1251
@@ -116,8 +114,8 @@ int main(array<System::String ^> ^args)
 		case 2: match(swimmers); break;
 		case 3: kolvo(swimmers); break;
 		case 4: best_on_dist(swimmers); break;
-		case 5: alfalist(swimmers); break;
-		case 6: diagram(swimmers); break;
+		case 5: alfalist(swimmers, spisok); break;
+		case 6: diagram(swimmers, spisok); break;
 		case 7: exit(0);
 		}
 	} // конец while(1)...
@@ -126,7 +124,17 @@ int main(array<System::String ^> ^args)
   
 int menu(int n)
 {
-	int y1 = 0, y2 = n - 1; //устанавливаем выделение для строчки
+	char dan[7][55] = { 
+ "Какой пловец набрал больше всего очков?               ",
+ "Есть ли пловцы, набравшие одинаковое количество очков?",
+ "Сколько пловцов улучшили свои результаты?             ",
+ "Кто стал лучшим на дистанции:                         ",
+ "Алфавитный список участников соревнований             ",
+ "Диаграмма по набранным очкам в процентном соотношении ",
+ "Выход                                                 "}; 
+ // Массив из пробелов для оформления
+    char BlankLine[]="                                                        "; 
+    int y1 = 0, y2 = n - 1; //устанавливаем выделение для строчки
 	char c = 1;
 	while (c != ESC) //при нажатии кнопки ESC, меню закроется
 	{
@@ -161,6 +169,13 @@ int menu(int n)
 // Функция поиска максимума
 void maxim(struct z* swimmers){
 	int i=0, NC; struct z best;
+	FILE *in; // на локальном уровне осуществляем открытие файла
+	if((in=fopen("results.dat","r"))==NULL) //попытка открыть файл
+	{
+		printf("\nФайл results.dat не открыт !");
+		getch(); exit(1);
+	}
+	fscanf(in, "%d", &NC);
 	strcpy(best.name,swimmers[0].name);
 	best.points = swimmers[0].points; //в переменную структуры записываем нулевой элемент
 	for(i=1;i<NC;i++)
@@ -178,6 +193,7 @@ void maxim(struct z* swimmers){
 		Console::CursorTop=16; //задаем область, где будет выведено макс.количество
 		printf("имеет в сумме %d очков!",best.points); // Вывод максимального количества
 		getch();
+		fclose(in);
 }
 // вспомогательная функция преобразования времени из строки в число
 float text_to_time(char *time) // char 1:14:06 -> int 74.06 s
@@ -195,7 +211,13 @@ void kolvo(struct z *swimmers)
 {
 	int i, k=0, NC;
 	float prev_res, new_res;
-
+	FILE *in; // на локальном уровне осуществляем открытие файла
+	if((in=fopen("results.dat","r"))==NULL) //попытка открыть файл
+	{
+		printf("\nФайл results.dat не открыт !");
+		getch(); exit(1);
+	}
+	fscanf(in, "%d", &NC);
 	for(i=0;i<NC;i++)
 	{
 		prev_res = text_to_time(swimmers[i].prev_time);
@@ -208,6 +230,7 @@ void kolvo(struct z *swimmers)
 	Console::CursorTop=15;
 	printf("%d из 27 участников улучшили свой результат.", k);
 	getch();
+	fclose(in);
 }
 
 // Найти спортсменов, набравших одинаковое количество очков
@@ -217,6 +240,13 @@ void match(struct z *swimmers)
 	char name_of_match[20] = "";
 	int i, j, NC;
 	struct z examp;
+	FILE *in; // на локальном уровне осуществляем открытие файла
+	if((in=fopen("results.dat","r"))==NULL) //попытка открыть файл
+	{
+		printf("\nФайл results.dat не открыт !");
+		getch(); exit(1);
+	}
+	fscanf(in, "%d", &NC);
 	for(j=0;j<NC;j++)
 	{
 		strcpy(examp.name, swimmers[j].name);
@@ -239,17 +269,25 @@ void match(struct z *swimmers)
 	Console::CursorTop=16;
 	printf("набрали %s и %s", examp.name, name_of_match);
 	getch();
+	fclose(in);
 }
 
 // Функция вывода алфавитного и обратного списка
-void alfalist(struct z* swimmers)
+void alfalist(struct z* swimmers, struct sp* spisok)
 {
-	int i, x=3, NC
+	int i, x=3, NC;
 	struct sp* nt, *z;
+	FILE *in; // на локальном уровне осуществляем открытие файла
+	if((in=fopen("results.dat","r"))==NULL) //попытка открыть файл
+	{
+		printf("\nФайл results.dat не открыт !");
+		getch(); exit(1);
+	}
+	fscanf(in, "%d", &NC);
 	Console::ForegroundColor=ConsoleColor::DarkMagenta;
 	Console::BackgroundColor=ConsoleColor::Gray;
 	if(!spisok)
-		for(i=0;i<NC;i++) vstavka(swimmers,swimmers[i].name);
+		for(i=0;i<NC;i++) vstavka(swimmers, swimmers[i].name, &spisok);
 	Console::Clear();
 	printf("\n Алфавитный список участников:\n");
 	printf(" ===============================");
@@ -276,14 +314,22 @@ void alfalist(struct z* swimmers)
 		x += 1;
 	}
 	getch();
+	fclose(in);
 }
 
 // Функция вставки элемента в связный список
-void vstavka(struct z* swimmers, char* fio)
+void vstavka(struct z* swimmers, char* fio, struct sp** spisok)
 {
 	int i, NC;
+	FILE *in; // на локальном уровне осуществляем открытие файла
+	if((in=fopen("results.dat","r"))==NULL) //попытка открыть файл
+	{
+		printf("\nФайл results.dat не открыт !");
+		getch(); exit(1);
+	}
+	fscanf(in, "%d", &NC);
 	struct sp *nov,*nt,*z=0;
-	for(nt=spisok; nt!=0 && strcmp(nt->fio,fio)<0; z=nt, nt=nt->sled);
+	for(nt=*spisok; nt!=0 && strcmp(nt->fio,fio)<0; z=nt, nt=nt->sled);
 	if(nt && strcmp(nt->fio,fio)==0) return;
 	nov=(struct sp *) malloc(sizeof(struct sp));
 	strcpy(nov->fio,fio);
@@ -292,18 +338,26 @@ void vstavka(struct z* swimmers, char* fio)
 	nov->points=0;
 	for(i=0;i<NC;i++)
 		if(strcmp(swimmers[i].name,fio)==0) nov->points=swimmers[i].points;
-	if(!z) spisok=nov;
+	if(!z) *spisok=nov;
 	else z->sled=nov;
 	if(nt) nt->pred=nov;
+	fclose(in);
 	return;
 }
 
 // Функция построения диаграммы
-void diagram(struct z *swimmers)
+void diagram(struct z *swimmers, struct sp* spisok)
 {
 	struct sp *nt;
 	int len, i, NColor;
 	int ochki = 0, NC;
+	FILE *in; // на локальном уровне осуществляем открытие файла
+	if((in=fopen("results.dat","r"))==NULL) //попытка открыть файл
+	{
+		printf("\nФайл results.dat не открыт !");
+		getch(); exit(1);
+	}
+	fscanf(in, "%d", &NC);
 	char str1[20];
 	char str2[20];
 	System::ConsoleColor Color;
@@ -312,7 +366,7 @@ void diagram(struct z *swimmers)
 	Console::Clear();
 	for(i=0;i<NC;i++) ochki = ochki + swimmers[i].points;
 	if(!spisok)
-		for(i=0;i<NC;i++) vstavka(swimmers,swimmers[i].name);
+		for(i=0;i<NC;i++) vstavka(swimmers, swimmers[i].name, &spisok);
 	Color=ConsoleColor::Black; NColor=0;
 	for(nt=spisok,i=0; nt!=0; nt=nt->sled,i++)
 	{
@@ -331,6 +385,7 @@ void diagram(struct z *swimmers)
 		{ Color=ConsoleColor::Black; NColor=0; }
 	}
 	getch();
+	fclose(in);
 	return ;
 }
 
@@ -341,6 +396,13 @@ void best_on_dist(struct z *swimmers)
 	int i; float fastest_time; char name_of_fastest[20] = "";
 	char swimmer_is_found_flag = 0;
 	int meters; char style[7];
+	FILE *in; // на локальном уровне осуществляем открытие файла
+	if((in=fopen("results.dat","r"))==NULL) //попытка открыть файл
+	{
+		printf("\nФайл results.dat не открыт !");
+		getch(); exit(1);
+	}
+	fscanf(in, "%d", &NC);
 	Console::ForegroundColor=ConsoleColor::DarkMagenta;
 	Console::BackgroundColor=ConsoleColor::Gray;
 	Console::CursorLeft=10;
@@ -371,6 +433,7 @@ void best_on_dist(struct z *swimmers)
 	}
 	printf("%s на дистанции %d %s", name_of_fastest, meters, style);
 	getch();
+	fclose(in);
 	return;
 }
 
